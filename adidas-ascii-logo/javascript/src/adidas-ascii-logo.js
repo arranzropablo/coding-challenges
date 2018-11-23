a42014dec76d6438ed277299d73684f1981b0913
Pablo Arranz Ropero <arranzropablo@gmail.com>
2018-11-23 11:11:20 +0100

    finished challenge

--- adidas-ascii-logo/javascript/src/adidas-ascii-logo.js
+++ adidas-ascii-logo/javascript/src/adidas-ascii-logo.js
@@ -4,5 +4,23 @@
  * @returns {string} adidas logo.
  */
 module.exports = function(width) {
-  return `${ width }`;
+  if (width < 2) {
+    throw 'Error, minimun width is 2';
+  }
+  let baseSize = Math.round(Math.sqrt(width));
+  let result = "";
+  for (let i = 0; i < baseSize; i++) {
+    result += " ".repeat((width * 2) + i) + printAts(width) + "\n";
+  }
+  for (let i = 0; i < baseSize; i++) { 
+    result += " ".repeat(width + i) + printAts(width) + " ".repeat(baseSize) + printAts(width) + "\n";
+  }
+  for (let i = 0; i < baseSize; i++) { 
+    result += " ".repeat(i) + printAts(width) + " ".repeat(baseSize) + printAts(width) + " ".repeat(baseSize) + printAts(width) + "\n";
+  }
+  return result.substr(0, result.length - 1);
 };
+
+function printAts(width) {
+  return "@".repeat(width);
+}
